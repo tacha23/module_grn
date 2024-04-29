4005b52a7264ead89101a46af16e578c4bfbed42
tacha23 <tanmay.chaturvedi@gmail.com>
2024-04-29 18:04:08 +0530

    WIP

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,14 +1,66 @@
-// src/app/movies/page.js
-
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table'
 
 // import MoviesFromDB from '../../../pages/api/movies';
 
 const MoviesPage = () => {
   // State to store movies data
   const [moviesData, setMoviesData] = useState([]);
+
+  const data = [
+    {
+      title: 'First',
+      year: '2011',
+      plot: 'very small',
+      rated: 'U'
+    },
+    {
+      title: 'Seconf',
+      year: '2012',
+      plot: 'small',
+      rated: 'U'
+    },
+    {
+      title: 'Third',
+      year: '2013',
+      plot: 'big',
+      rated: 'A'
+    },
+    {
+      title: 'Fourth',
+      year: '2014',
+      plot: 'very big',
+      rated: 'R'
+    },
+  ]
+
+  const columns = useMemo(
+    () => [
+      {
+        header: 'Title',
+        accessorKey: 'title',
+        enableHiding: false
+      },
+      {
+        header: 'Year',
+        accessorKey: 'year',
+        enableHiding: false
+      },
+      {
+        header: 'Plot',
+        accessorKey: 'plot',
+        enableHiding: false
+      },
+      {
+        header: 'Rated',
+        accessorKey: 'rated',
+        enableHiding: false
+      }
+    ]
+  )
 
   // Fetch movies data on component mount
   useEffect(() => {
@@ -30,6 +82,14 @@ const MoviesPage = () => {
     fetchData();
   }, []);
 
+  const table = useMaterialReactTable({
+    enableStickyHeader: true,
+    enableStickyFooter: true,
+    columns,
+    data,
+    // mrtTheme.baseBackgroundColor: 'blue'
+  });
+
   return (
     <div>
       <h1>Movies Page</h1>
@@ -39,6 +99,7 @@ const MoviesPage = () => {
           <li key={movie._id}>{movie.title}</li>
         ))}
       </ul>
+      <MaterialReactTable table={table} />;
     </div>
   );
 };
